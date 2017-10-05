fe91d282c068f7ac224713b64257ead1205bf0d6
seitahyi <seitahyi@naver.com>
2017-10-05 12:31:56 +0900

    v0.1.1

--- src/package_calendar/Calendar.java
+++ src/package_calendar/Calendar.java
@@ -5,8 +5,11 @@ public class Calendar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello world");
-
+		System.out.println("일 월 화 수 목 금 토 일");
+		System.out.println("--------------");
+		System.out.println("1 2 3 4 5 6 7 ");
+		System.out.println("8 9 10 11 12 13 ");
+		System.out.println("14 15 16 17 18 19");
 	}
 
 }
