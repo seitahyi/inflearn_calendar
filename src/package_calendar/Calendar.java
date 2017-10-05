f2d49e24017d12dc505bc797be4f2c8ac85cb119
seitahyi <seitahyi@naver.com>
2017-10-05 13:17:54 +0900

    v0.1.3

--- src/package_calendar/Calendar.java
+++ src/package_calendar/Calendar.java
@@ -6,27 +6,38 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		
-		System.out.println("일 월 화 수 목 금 토 일");
-		System.out.println("--------------");
-		System.out.println("1 2 3 4 5 6 7 ");
-		System.out.println("8 9 10 11 12 13 ");
-		System.out.println("14 15 16 17 18 19");
-		
-		// 숫자 입력받아 해당 달의 최대 일수 출력
-		
-		int a, b;
+//		System.out.println("일 월 화 수 목 금 토 일");
+//		System.out.println("--------------");
+//		System.out.println("1 2 3 4 5 6 7 ");
+//		System.out.println("8 9 10 11 12 13 ");
+//		System.out.println("14 15 16 17 18 19");
+//		
+//		// 숫자 입력받아 해당 달의 최대 일수 출력
+//		
+//		int a, b;
+//		
+//		Scanner scan = new Scanner(System.in);
+//		
+//		String	s1, s2;
+//		
+//		s1	= scan.next();
+//		s2	= scan.next();
+//		
+//		a	= Integer.parseInt(s1);
+//		b	= Integer.parseInt(s2);
+//		
+//		System.out.printf("%d %d", a + b);
+//		
+//		scan.close();
 		
 		Scanner scan = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
 		
-		String	s1, s2;
+		int month = scan.nextInt();
 		
-		s1	= scan.next();
-		s2	= scan.next();
+		int[] maxDays = {31, 28, 31, 30, 31, 30};
 		
-		a	= Integer.parseInt(s1);
-		b	= Integer.parseInt(s2);
-		
-		System.out.printf("%d %d", a + b);
+		System.out.printf("%d 월은 %d일 깢 있음\n", month, maxDays[month - 1]);
 		
 		scan.close();
 	}
