7e4cd89eee152f38d3a51ceef4fac6f3fcccae7a
seitahyi <seitahyi@naver.com>
2017-10-05 15:27:44 +0900

    v0.1.5.1

--- src/package_calendar/Calendar.java
+++ src/package_calendar/Calendar.java
@@ -23,17 +23,26 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		
+		String PROMPT = "cal> ";
+		
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar(); // class 스스로를 불러옴.
 		
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scan.nextInt();
-		
-		for(int i = 0; i < repeat; i++){
+		int month = 1;
+		while(true){
 			
 			System.out.println("달을 입력하세요");
-			int month = scan.nextInt();
-			int[] maxDays = {31, 28, 31, 30, 31, 30};
+			System.out.println(PROMPT);
+			month = scan.nextInt();
+			
+			if(month == -1){
+				break;
+			}
+			
+			if(month > 12){
+				continue;
+			}
+			
 			System.out.printf("%d 월은 %d일 깢 있음\n", month, cal.getMaxDaysOfMonth(month));
 			
 		}
