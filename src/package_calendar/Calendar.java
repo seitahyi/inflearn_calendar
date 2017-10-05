3ca088b0f833cb558a232094aa2fb3c12005c18a
seitahyi <seitahyi@naver.com>
2017-10-05 12:17:09 +0900

    v.0.1

---
+++ src/package_calendar/Calendar.java
@@ -0,0 +1,12 @@
+package package_calendar;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
